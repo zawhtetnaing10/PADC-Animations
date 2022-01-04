5ee3a5040c1e712bb0c20468c7c2f32b202e8217
Zaw Htet Naing <zawhtet.naing@mm-digital-solutions.com>
2022-01-04 10:23:38 +0630

    Adding Tween Animation for Single Property

--- padc_animations/lib/implicit_animations/implicit_animations.dart
+++ padc_animations/lib/implicit_animations/implicit_animations.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:padc_animations/utils/dimens.dart';
 
 const kAnimationDuration = const Duration(milliseconds: 500);
+const kAnimationDurationForScreenFadeIn = const Duration(milliseconds: 1000);
 
 class ImplicitAnimations extends StatefulWidget {
   const ImplicitAnimations({Key? key}) : super(key: key);
@@ -19,133 +20,141 @@ class _ImplicitAnimationsState extends State<ImplicitAnimations> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: AnimatedContainer(
-        duration: kAnimationDuration,
-        color: (isChangeBackgroundColor) ? Colors.black : Colors.white,
-        child: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              AnimatedContainer(
-                width: (isNewDimensions) ? 300 : 200,
-                height: (isNewDimensions) ? 300 : 200,
-                duration: kAnimationDuration,
-                curve: Curves.easeInCubic,
-                child: Image.asset(
-                  "assets/bird_image.png",
-                  fit: BoxFit.cover,
-                ),
-              ),
-              AnimatedSize(
-                duration: kAnimationDuration,
-                child: Container(
-                  height: isDescriptionShown ? null : 0.0,
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        height: kMarginMedium2,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: kMarginMedium2),
-                        child: Text(
-                          "Birds are vertebrates (animals with backbones) with wings and feathers. Most birds can fly, using powerful muscles to flap their wings. ... Birds' bodies are covered with a light, tough layer of feathers and they have very light skeletons. Instead of teeth, they have hornlike beaks, or bills.",
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            color: isChangeBackgroundColor
-                                ? Colors.white
-                                : Colors.black,
-                          ),
-                        ),
-                      ),
-                    ],
+      body: TweenAnimationBuilder(
+        child: AnimatedContainer(
+          duration: kAnimationDuration,
+          color: (isChangeBackgroundColor) ? Colors.black : Colors.white,
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                AnimatedContainer(
+                  width: (isNewDimensions) ? 300 : 200,
+                  height: (isNewDimensions) ? 300 : 200,
+                  duration: kAnimationDuration,
+                  curve: Curves.easeInCubic,
+                  child: Image.asset(
+                    "assets/bird_image.png",
+                    fit: BoxFit.cover,
                   ),
                 ),
-              ),
-              SizedBox(height: kMarginMedium2),
-              AnimatedSize(
-                duration: kAnimationDuration,
-                child: AnimatedSwitcher(
+                AnimatedSize(
                   duration: kAnimationDuration,
-                  child: (isChangeButtonsLayout)
-                      ? Wrap(
-                          alignment: WrapAlignment.center,
-                          spacing: kMarginMedium2,
-                          children: [
-                            PrimaryButton(
-                              onTap: () {
-                                setState(() {
-                                  isNewDimensions = !isNewDimensions;
-                                });
-                              },
-                              label: "Change Dimensions",
-                            ),
-                            PrimaryButton(
-                              onTap: () {
-                                setState(() {
-                                  isDescriptionShown = !isDescriptionShown;
-                                });
-                              },
-                              label: (isDescriptionShown)
-                                  ? "Hide Description"
-                                  : "Show Description",
-                            ),
-                            PrimaryButton(
-                              onTap: () {
-                                setState(() {
-                                  isChangeBackgroundColor =
-                                      !isChangeBackgroundColor;
-                                });
-                              },
-                              label: "Change Theme",
-                            ),
-                          ],
-                        )
-                      : Column(
-                          children: [
-                            PrimaryButton(
-                              onTap: () {
-                                setState(() {
-                                  isNewDimensions = !isNewDimensions;
-                                });
-                              },
-                              label: "Change Dimensions",
-                            ),
-                            PrimaryButton(
-                              onTap: () {
-                                setState(() {
-                                  isDescriptionShown = !isDescriptionShown;
-                                });
-                              },
-                              label: (isDescriptionShown)
-                                  ? "Hide Description"
-                                  : "Show Description",
-                            ),
-                            PrimaryButton(
-                              onTap: () {
-                                setState(() {
-                                  isChangeBackgroundColor =
-                                      !isChangeBackgroundColor;
-                                });
-                              },
-                              label: "Change Theme",
-                            ),
-                          ],
+                  child: Container(
+                    height: isDescriptionShown ? null : 0.0,
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          height: kMarginMedium2,
                         ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: kMarginMedium2),
+                          child: Text(
+                            "Birds are vertebrates (animals with backbones) with wings and feathers. Most birds can fly, using powerful muscles to flap their wings. ... Birds' bodies are covered with a light, tough layer of feathers and they have very light skeletons. Instead of teeth, they have hornlike beaks, or bills.",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: isChangeBackgroundColor
+                                  ? Colors.white
+                                  : Colors.black,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
-              ),
-              SizedBox(height: kMarginMedium2),
-              PrimaryButton(
-                onTap: () {
-                  setState(() {
-                    isChangeButtonsLayout = !isChangeButtonsLayout;
-                  });
-                },
-                label: "Change Button Layout",
-                color: Colors.red,
-              ),
-            ],
+                SizedBox(height: kMarginMedium2),
+                AnimatedSize(
+                  duration: kAnimationDuration,
+                  child: AnimatedSwitcher(
+                    duration: kAnimationDuration,
+                    child: (isChangeButtonsLayout)
+                        ? Wrap(
+                            alignment: WrapAlignment.center,
+                            spacing: kMarginMedium2,
+                            children: [
+                              PrimaryButton(
+                                onTap: () {
+                                  setState(() {
+                                    isNewDimensions = !isNewDimensions;
+                                  });
+                                },
+                                label: "Change Dimensions",
+                              ),
+                              PrimaryButton(
+                                onTap: () {
+                                  setState(() {
+                                    isDescriptionShown = !isDescriptionShown;
+                                  });
+                                },
+                                label: (isDescriptionShown)
+                                    ? "Hide Description"
+                                    : "Show Description",
+                              ),
+                              PrimaryButton(
+                                onTap: () {
+                                  setState(() {
+                                    isChangeBackgroundColor =
+                                        !isChangeBackgroundColor;
+                                  });
+                                },
+                                label: "Change Theme",
+                              ),
+                            ],
+                          )
+                        : Column(
+                            children: [
+                              PrimaryButton(
+                                onTap: () {
+                                  setState(() {
+                                    isNewDimensions = !isNewDimensions;
+                                  });
+                                },
+                                label: "Change Dimensions",
+                              ),
+                              PrimaryButton(
+                                onTap: () {
+                                  setState(() {
+                                    isDescriptionShown = !isDescriptionShown;
+                                  });
+                                },
+                                label: (isDescriptionShown)
+                                    ? "Hide Description"
+                                    : "Show Description",
+                              ),
+                              PrimaryButton(
+                                onTap: () {
+                                  setState(() {
+                                    isChangeBackgroundColor =
+                                        !isChangeBackgroundColor;
+                                  });
+                                },
+                                label: "Change Theme",
+                              ),
+                            ],
+                          ),
+                  ),
+                ),
+                SizedBox(height: kMarginMedium2),
+                PrimaryButton(
+                  onTap: () {
+                    setState(() {
+                      isChangeButtonsLayout = !isChangeButtonsLayout;
+                    });
+                  },
+                  label: "Change Button Layout",
+                  color: Colors.red,
+                ),
+              ],
+            ),
           ),
+        ),
+        duration: kAnimationDurationForScreenFadeIn,
+        tween: Tween<double>(begin: 0, end: 1),
+        builder: (context, double _opacity, child) => Opacity(
+          opacity: _opacity,
+          child: child,
         ),
       ),
     );
